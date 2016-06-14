52755d5abbe047443dd54469b5246ca07370a265
Boubacar Diarra <boubadiarra@gmail.com>
2016-06-14 23:36:08 +0200

    initial import

---
+++ geninfo/tests/geninfo.cpp
@@ -0,0 +1,11 @@
+//============================================================================
+// Name        : geninfo.cpp
+// Author      : Boubacar Diarra
+// Version     :
+// Copyright   : boubadiarra@gmail.com
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#define BOOST_TEST_MODULE "C++ Unit Tests for statdata"
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+/////////////////////////////
--- infostat/include/activeobject.h
+++ infostat/include/activeobject.h
@@ -8,20 +8,53 @@
 #ifndef ACTIVEOBJECT_H_
 #define ACTIVEOBJECT_H_
 //////////////////////////
-#include "info_includes.h"
-#include "sharedqueue.h"
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <functional>
+#include <atomic>
 /////////////////////////////
 namespace info {
 ////////////////////////
-typedef std::function<void()> Operation;
+template <typename T>
+class DispatchQueue {
+	std::mutex qlock;
+	std::queue<T> ops_queue;
+	std::condition_variable empty;
+public:
+	DispatchQueue() {}
+	virtual ~DispatchQueue() {}
+public:
+	void clear(void) {
+		std::lock_guard<std::mutex> guard(qlock);
+		while (!ops_queue.empty()) {
+			ops_queue.pop();
+		}
+	}// clear
+	void put(T op) {
+		std::lock_guard<std::mutex> guard(qlock);
+		ops_queue.push(op);
+		empty.notify_one();
+	} // put
+	T take() {
+		std::unique_lock<std::mutex> lock(qlock);
+		empty.wait(lock, [&] {return !ops_queue.empty(); });
+		T op = ops_queue.front();
+		ops_queue.pop();
+		return op;
+	} // take
+};
 /////////////////////////
 class Active {
+public:
+	using Operation = std::function<void()>;
+private:
 	std::atomic<bool> done;
 	DispatchQueue<Operation> dispatchQueue;
 	std::thread runnable;
 private:
-	Active() :
-			done(false) {
+	Active() : done(false) {
 	} // Active
 	Active(const Active&) = delete;
 	Active& operator=(const Active&) = delete;
@@ -33,12 +66,21 @@ private:
 public:
 	~Active() {
 		// Schedule a No-Op runnable to flush the dispatch queue
+		this->done.store(true);
 		dispatchQueue.put([this]() {this->done.store(true);});
 		runnable.join();
 	} // run
+	void stop(void) {
+		this->done.store(true);
+		dispatchQueue.put([this]() {this->done.store(true); });
+		runnable.join();
+	}
 	void send(Operation msg_) {
 		this->dispatchQueue.put(msg_);
 	} // send
+	void clear(void) {
+		this->dispatchQueue.clear();
+	}
 	  // Factory: safe construction of object before thread start
 	static std::unique_ptr<Active> createActive(void) {
 		std::unique_ptr<Active> aPtr(new Active());
@@ -49,17 +91,23 @@ public:
 // class ActiveObject
 //////////////////////////////////
 class Backgrounder {
+public:
+	using Operation = std::function<void()>;
 private:
 	std::unique_ptr<Active> _active;
 protected:
 	void put(Operation op) {
 		this->_active->send(op);
 	}	 // put
+	void clear() {
+		this->_active->clear();
+	}// clear
 public:
-	Backgrounder() :
-			_active(Active::createActive()) {
+	Backgrounder(){
+		this->_active = Active::createActive();
 	}
 	virtual ~Backgrounder() {
+		this->_active.reset();
 	}
 	void send(Operation op) {
 		this->_active->send(op);
